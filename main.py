2c15827b3b62d82aba342f63f0d0effa7feac79f
BarrilBaril <79324037+BarrilBaril@users.noreply.github.com>
2021-02-22 14:06:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import tkinter as tk
 from PIL import Image, ImageTk
 # Estes imports só servem para editar o icon (pelo menos por enquanto)
 import tempfile, base64, zlib
+from tkinter import ttk
+
+count= 0
 
 class MainWindow(tk.Frame):
     def __init__(self, master=None):
@@ -54,8 +57,10 @@ class MainWindow(tk.Frame):
         self.place_item("./img/Image_white.PNG", 0.25, self.logo_canvas)
         self.create_widgets()
         
+        global count
         # Para garantir que os widgets e imagens mudam de tamanho
-        self.master.bind('<Configure>', self._resize_window)
+        if(count == 0):
+            self.master.bind('<Configure>', self._resize_window)
     
     def place_item(self, src, ratio, canvas):
         """
@@ -122,10 +127,89 @@ class MainWindow(tk.Frame):
         self.old.bind("<Leave>", func=lambda e: self.old.config(bg='red',fg='white'))
 
     def say_import(self):
+        self.title_canvas.delete("all")
+        self.logo_canvas.delete("all")
+        self.old.destroy()
+        self.new.destroy()
+        global count
+        count = 1
         print("Import fit")
+        self.master.configure(background='#FCF6F5')
+        
     
     def say_new(self):
         print("Create new fit")
+        self.title_canvas.delete("all")
+        self.logo_canvas.delete("all")
+        self.old.destroy()
+        self.new.destroy()
+        global count
+        count = 1
+        self.master.configure(background='#FCF6F5')
+        
+        self.frameleft = tk.Frame(self.master,  bg='#FCF6F5')
+        self.frameleft.place( in_ = self.master, relwidth =0.5, relheight = 1,relx=0, rely=0)
+        
+        self.frameright = tk.Frame(self.master,  bg='#FCF6F5')
+        self.frameright.place( in_ = self.master, relwidth=0.5, relheight=1,relx=0.5, rely=0)
+        
+        self.subframeright1=tk.Frame(self.frameright, bg = '#FCF6F5')
+        self.subframeright1.place(in_ = self.frameright, relwidth=1, relheight=0.5, relx=0, rely=0)
+        
+        self.textentry = tk.Label(self.subframeright1,text= "Parameter")
+        self.textentry.place(relwidth=0.65, rely=0.0, relheight=0.05)
+        self.entry = tk.Entry(self.subframeright1, font=40)
+        self.entry.place(relwidth=0.55, rely=0.05, relheight=0.05)
+        self.entry.focus_set()
+        self.upbutton = tk.Button(self.subframeright1, text= "update")
+        self.upbutton.place(relwidth=0.1,relx=0.55, rely=0.05,relheight=0.05 )
+        self.upbutton["command"] = self.update_parameter
+        
+        self.subframeright2=tk.Frame(self.frameright, bg = '#FCF6F5')
+        self.subframeright2.place(in_ = self.frameright, relwidth=1, relheight=0.5, relx=0, rely=0.5)
+    
+    def update_parameter(self):
+        self.parameter = self.entry.get()
+        first_split = self.parameter.split(' ')
+        clean_split = []
+        for val in first_split:
+            for param in val.split(','):
+                if param:
+                    clean_split.append(param)
+                    
+        boxnumber = len(clean_split)
+        self.paramboxes=[]
+        self.paramlabel=[]
+        self.paramcanvas = tk.Canvas(self.subframeright2, highlightthickness=0)
+        self.paramcanvas.pack(side=tk.LEFT, fill = tk.BOTH, expand=1)
+        
+        
+        self.paramscrolly = ttk.Scrollbar(self.subframeright2, orient = "vertical", command=self.paramcanvas.yview)
+        self.paramscrolly.pack(side=tk.RIGHT, fill="y")
+        
+        self.paramcanvas.configure(yscrollcommand=self.paramscrolly.set)
+        self.paramcanvas.bind('<Configure>', lambda e: self.paramcanvas.configure(scrollregion = self.paramcanvas.bbox("all")))
+        
+        self.anotherframe=tk.Frame(self.paramcanvas)
+        
+        
+        self.paramcanvas.create_window((0,0), window = self.anotherframe, anchor = "nw")
+        
+        
+        for x in range(boxnumber):
+            self.paramboxes.append(tk.Entry(self.anotherframe, font=40))
+            self.paramlabel.append(tk.Label(self.anotherframe, text = clean_split[x]))
+            
+        
+        for x in range(boxnumber):
+            self.paramlabel[x].grid(column = 0, row = x, pady=10)
+            self.paramboxes[x].grid(column = 1, row = x, pady=10)
+        
+        self.anotherframe.grid_columnconfigure(0,weight=1)
+        self.anotherframe.grid_columnconfigure(1,weight=1)
+            
+
+        
         
 root = tk.Tk()
 app = MainWindow(master=root)
