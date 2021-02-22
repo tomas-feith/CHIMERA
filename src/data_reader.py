913ae6a45b0759e9fd1fb26a7b286d0a85329f58
tomasfeith-IST <tomas.feith@tecnico.ulisboa.pt>
2021-02-22 12:27:15 +0000

    generalização do data_reader completa

--- src/data_reader.py
+++ src/data_reader.py
@@ -37,10 +37,11 @@ def read_file(src, out):
         return -1
     # Se for da classe ficheiro de texto
     if form:
-        data = pd.read_csv(src, sep=r"\s+|;|:|None|,", engine='python', header=None, dtype="object")
+        data = pd.read_csv(src, sep=r"\s+|;|:|None|,", engine='python', dtype="object", header=None)
     # Se for da classe Excel
     else:
-        data = pd.read_excel(src, dtype="object", header=None)
+        data = pd.read_excel(src, dtype="object",header=None)
+    print(data)
 
     # Fazer a divisão nos datasets fornecidos
     # Se não houver incerteza no x, então o número de colunas é ímpar
@@ -61,20 +62,46 @@ def read_file(src, out):
                     return -2
                 # Se a linha estiver vazia não se acrescenta
                 if (
+                        not (out==float and np.isnan(x)) and
+                        not (out==str and x=='nan') and
                         not (out==float and np.isnan(y) and np.isnan(ey)) and
-                        not (out==str and y == 'nan' and ey == 'nan')
+                        not (out==str and y=='nan' and ey=='nan')
                     ):
                     points.append([x, y, ey])
             full_sets.append(points)
-        print(full_sets[0])
-        print(full_sets[1])
     # Se houver incerteza no x o tratamento é ligeiramente diferente
     else:
-        pass
-
+        for i in range(1,int((data.shape[1])/2)):
+            points = []
+            for j in range(len(data[2*i].to_numpy())):
+                x = data[0].to_numpy(out)[j]
+                ex = data[1].to_numpt(out)[j]
+                y = data[2*i].to_numpy(out)[j]
+                ey = data[2*i+1].to_numpy(out)[j]
+                # Procurar incoerências nas linhas
+                if (
+                        (out==float and np.isnan(x)!=np.isnan(ex)) or
+                        (out==str and x!='nan' and ex!='nan') or
+                        (out==float and np.isnan(y)!=np.isnan(ey)) or
+                        (out==str and y=='nan' and ey!='nan') or
+                        (out==str and y!='nan'and ey=='nan')
+                    ):
+                    return -3
+                # Se a linha estiver vazia não se acrescenta
+                if (
+                        not (out==float and np.isnan(x) and np.isnan(ex)) and 
+                        not (out==str and x=='nan' and ex=='nan') and
+                        not (out==float and np.isnan(y) and np.isnan(ey)) and
+                        not (out==str and y=='nan' and ey=='nan')
+                    ):
+                    points.append([x, ex, y, ey])
+            full_sets.append(points)
+    
+    print(full_sets[0])
+    print(full_sets[1])
     return full_sets
 
 if __name__ == '__main__':
-    read_file('../../test_data.txt', float)
-    # read_file('../../test_data.xlsx', float)
+    # read_file('../../test_data.txt', float)
+    read_file('../../test_data.xlsx', float)
     
