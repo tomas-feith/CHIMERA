7cd2d3f0c2b271227b0eb2d2f68cf8af138f697e
tomasfeith-IST <tomas.feith@tecnico.ulisboa.pt>
2021-02-26 11:06:00 +0000

    Pôr fundos em tudo + seguranças em update_params

--- main.py
+++ main.py
@@ -26,10 +26,8 @@ from io import StringIO
 # Isto serve para quê?
 count = 0
 
-def parser(expr, params, indep):
-    np.seterr(all='raise')
-    # Funções do numpy a utilizar
-    # Ainda falta acrescentar as funções de estatística
+def process_params(params, indep):
+    allowed = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
     functions = ['sin',
                  'cos',
                  'tan',
@@ -44,11 +42,7 @@ def parser(expr, params, indep):
                  'cbrt',
                  'sign'
                  ]
-
-    # Lista de caraters permitidos nos parametros/variáveis
-    # Apenas letras e números
-    allowed = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
-
+    
     # Fazer limpeza dos params
     # Assumindo que estão separados por virgulas ou espaços
     # Os parâmetros limpos serão guardados neste array
@@ -70,11 +64,6 @@ def parser(expr, params, indep):
     indep=indep.replace(' ','')
     if indep == '':
         return (False, 'Não foi encontrada nenhuma variável independente.')
-    # Ver também se a função não está vazia
-    expr=expr.replace(' ','')
-    if expr == '':
-        return (False, 'Não foi encontrada nenhuma função de ajustamento.')
-
     # Ver se algum dos parâmetros tem carateres proibidos
     for val in clean_split:
         for char in val:
@@ -115,6 +104,41 @@ def parser(expr, params, indep):
     # Igual a acima
     else:
         return (False, 'A variável independente dada \''+str(indep)+'\' é um número. Utilize uma diferente.')
+    
+    return (True, clean_split)
+    
+
+def parser(expr, params, indep):
+    np.seterr(all='raise')
+    # Funções do numpy a utilizar
+    # Ainda falta acrescentar as funções de estatística
+    functions = ['sin',
+                 'cos',
+                 'tan',
+                 'arcsin',
+                 'arccos',
+                 'arctan',
+                 'exp',
+                 'log',
+                 'sqrt',
+                 'absolute',
+                 'heaviside',
+                 'cbrt',
+                 'sign'
+                 ]
+    
+    # Ver se a função não está vazia
+    expr=expr.replace(' ','')
+    if expr == '':
+        return (False, 'Não foi encontrada nenhuma função de ajustamento.')
+
+    process = process_params(params, indep)
+    print(process)
+    if process[0]:
+        clean_split = process[1]
+    else:
+        return (False, process[1])
+    
 
     # Substituir as funções pelo equivalente numpy
     # Primeira substituição temporária para não haver erros de conversão
@@ -128,6 +152,8 @@ def parser(expr, params, indep):
 
     # Substituir a variável independente
     expr = expr.split(indep)
+    for i in enumerate(expr):
+        expr[i[0]]=expr[i[0]].replace('x','X')
     expr = 'x'.join(expr)
 
     # Voltar a substituir os elementos pelas funções
@@ -137,6 +163,7 @@ def parser(expr, params, indep):
 
     # Vamos finalmente testar se a função funciona
     # Valores de teste só porque sim
+    print(expr)
     B = [np.pi/2]*len(clean_split)
     x=-1
     try:
@@ -391,7 +418,7 @@ class MainWindow(tk.Frame):
         self.subframeright1.place(in_=self.frameright, relwidth=1, relheight=0.5, relx=0, rely=0)
 
         # Criação da zona para inserir a variável independente
-        self.independentlabel = tk.Label(self.subframeright1,text="Independent Variable")
+        self.independentlabel = tk.Label(self.subframeright1,text="Independent Variable", bg='#FCF6F5')
         self.independentlabel["font"] = ("Roboto",int(15*1000/self.master.winfo_width()))
         self.independentlabel.place(relwidth=0.2, rely=0, relheight=0.1)
         self.independententry = tk.Entry(self.subframeright1, font=40)
@@ -399,7 +426,7 @@ class MainWindow(tk.Frame):
         self.independententry.focus_set()
 
         # Criação da zona para inserir os parâmetros
-        self.parameterlabel = tk.Label(self.subframeright1,text="Parameter")
+        self.parameterlabel = tk.Label(self.subframeright1,text="Parameter", bg='#FCF6F5')
         self.parameterlabel["font"] = ("Roboto",int(15*1000/self.master.winfo_width()))
         self.parameterlabel.place(relwidth=0.2, rely=0.1, relheight=0.1)
         self.parameterentry = tk.Entry(self.subframeright1, font=40)
@@ -417,7 +444,7 @@ class MainWindow(tk.Frame):
         self.upbutton.bind("<Leave>", func=lambda e: self.upbutton.config(bg='red',fg='white'))
         self.upbutton["font"] = ("Roboto",int(20*1000/self.master.winfo_width()))
 
-        self.functionlabel = tk.Label(self.subframeright1,text= "Function")
+        self.functionlabel = tk.Label(self.subframeright1,text= "Function", bg='#FCF6F5')
         self.functionlabel["font"] = ("Roboto",int(15*1000/self.master.winfo_width()))
         self.functionlabel.place(relwidth=0.2, rely=0.2, relheight=0.1)
         self.functionentry = tk.Entry(self.subframeright1, font=40)
@@ -437,71 +464,71 @@ class MainWindow(tk.Frame):
 
 
         # Criação das frames para a edição visual do gráfico
-        self.subframeright2=tk.Frame(self.frameright, bg = '#FCF6F5')
+        self.subframeright2=tk.Frame(self.frameright, bg='#FCF6F5')
         self.subframeright2.place(in_ = self.frameright, relwidth=1, relheight=0.3, relx=0, rely=0.25)
 
-        self.subframeleft1=tk.Frame(self.frameleft, bg = '#FCF6F5')
+        self.subframeleft1=tk.Frame(self.frameleft, bg='#FCF6F5')
         self.subframeleft1.place(in_ = self.frameleft, relwidth=1, relheight=0.5, relx=0, rely=0)
 
-        self.subframeleft2 = tk.Frame(self.frameleft, bg = 'white')
+        self.subframeleft2 = tk.Frame(self.frameleft, bg='#FCF6F5')
         self.subframeleft2.place(in_ = self.frameleft, relwidth = 1, relheight= 0.5, relx=0, rely=0.5)
 
-        self.subframeright3 = tk.Frame(self.frameright)
+        self.subframeright3 = tk.Frame(self.frameright, bg='#FCF6F5')
         self.subframeright3.place(in_ = self.frameright, relwidth = 1, relheight = 0.40, rely=0.60)
 
-        self.xaxislabel = tk.Label(self.subframeright3, text= "X Axis")
+        self.xaxislabel = tk.Label(self.subframeright3, text="X Axis", bg='#FCF6F5')
         self.xaxislabel.place(in_ = self.subframeright3, relwidth = 0.5, relheight=0.1, relx=0, rely=0)
 
-        self.yaxislabel = tk.Label(self.subframeright3, text= "Y Axis")
+        self.yaxislabel = tk.Label(self.subframeright3, text="Y Axis", bg='#FCF6F5')
         self.yaxislabel.place(in_ = self.subframeright3, relwidth = 0.5, relheight=0.1, relx=0.5, rely=0)
 
-        self.xaxisrangelabel = tk.Label(self.subframeright3, text = "Range: from")
+        self.xaxisrangelabel = tk.Label(self.subframeright3, text = "Range: from", bg='#FCF6F5')
         self.xaxisrangelabel.place(in_ = self.subframeright3, relwidth=0.15, relheight=0.1, relx = 0, rely = 0.1)
 
-        self.xaxisminentry = tk.Entry(self.subframeright3)
+        self.xaxisminentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.xaxisminentry.place(in_ = self.subframeright3, relwidth = 0.1, relheight=0.1, relx=0.15, rely=0.1)
 
-        self.xaxistolabel = tk.Label(self.subframeright3, text = "to")
+        self.xaxistolabel = tk.Label(self.subframeright3, text = "to", bg='#FCF6F5')
         self.xaxistolabel.place(in_ = self.subframeright3, relwidth=0.05, relheight=0.1, relx=0.25, rely=0.1)
 
-        self.xaxismaxentry = tk.Entry(self.subframeright3)
+        self.xaxismaxentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.xaxismaxentry.place(in_ = self.subframeright3, relwidth = 0.1, relheight=0.1, relx=0.3, rely=0.1)
 
-        self.xaxistitlelabel = tk.Label(self.subframeright3, text = "Title")
+        self.xaxistitlelabel = tk.Label(self.subframeright3, text = "Title", bg='#FCF6F5')
         self.xaxistitlelabel.place(in_ = self.subframeright3, relwidth = 0.1, relheight = 0.1, relx = 0, rely=0.25)
 
-        self.xaxistitleentry = tk.Entry(self.subframeright3)
+        self.xaxistitleentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.xaxistitleentry.place(in_ = self.subframeright3, relwidth = 0.4, relheight = 0.1, relx = 0.1, rely=0.25)
 
-        self.xaxisticksplabel = tk.Label(self.subframeright3, text = "Tick Spacing")
+        self.xaxisticksplabel = tk.Label(self.subframeright3, text = "Tick Spacing", bg='#FCF6F5')
         self.xaxisticksplabel.place(in_=self.subframeright3, relwidth = 0.15, relheight = 0.1, relx=0, rely= 0.4)
 
-        self.xaxistickspentry = tk.Entry(self.subframeright3)
+        self.xaxistickspentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.xaxistickspentry.place(in_ = self.subframeright3, relwidth = 0.3, relheight = 0.1, relx = 0.15, rely=0.4)
 
 
-        self.yaxisrangelabel = tk.Label(self.subframeright3, text = "Range: from")
+        self.yaxisrangelabel = tk.Label(self.subframeright3, text = "Range: from", bg='#FCF6F5')
         self.yaxisrangelabel.place(in_ = self.subframeright3, relwidth=0.15, relheight=0.1, relx = 5, rely = 0.1)
 
-        self.yaxisminentry = tk.Entry(self.subframeright3)
+        self.yaxisminentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.yaxisminentry.place(in_ = self.subframeright3, relwidth = 0.1, relheight=0.1, relx=0.65, rely=0.1)
 
-        self.yaxistolabel = tk.Label(self.subframeright3, text = "to")
+        self.yaxistolabel = tk.Label(self.subframeright3, text = "to", bg='#FCF6F5')
         self.yaxistolabel.place(in_ = self.subframeright3, relwidth=0.05, relheight=0.1, relx=0.75, rely=0.1)
 
-        self.yaxismaxentry = tk.Entry(self.subframeright3)
+        self.yaxismaxentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.yaxismaxentry.place(in_ = self.subframeright3, relwidth = 0.1, relheight=0.1, relx=0.8, rely=0.1)
 
-        self.yaxistitlelabel = tk.Label(self.subframeright3, text = "Title")
+        self.yaxistitlelabel = tk.Label(self.subframeright3, text = "Title", bg='#FCF6F5')
         self.yaxistitlelabel.place(in_ = self.subframeright3, relwidth = 0.1, relheight = 0.1, relx = 0.5, rely=0.25)
 
-        self.yaxistitleentry = tk.Entry(self.subframeright3)
+        self.yaxistitleentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.yaxistitleentry.place(in_ = self.subframeright3, relwidth = 0.4, relheight = 0.1, relx = 0.6, rely=0.25)
 
-        self.yaxisticksplabel = tk.Label(self.subframeright3, text = "Tick Spacing")
+        self.yaxisticksplabel = tk.Label(self.subframeright3, text = "Tick Spacing", bg='#FCF6F5')
         self.yaxisticksplabel.place(in_=self.subframeright3, relwidth = 0.15, relheight = 0.1, relx=0.5, rely= 0.4)
 
-        self.yaxistickspentry = tk.Entry(self.subframeright3)
+        self.yaxistickspentry = tk.Entry(self.subframeright3, bg='#FCF6F5')
         self.yaxistickspentry.place(in_ = self.subframeright3, relwidth = 0.3, relheight = 0.1, relx = 0.65, rely=0.4)
 
 
@@ -607,106 +634,113 @@ class MainWindow(tk.Frame):
         self.canvas.draw()
 
     def update_parameter(self):
-        self.parameter = self.parameterentry.get()
-        first_split = self.parameter.split(' ')
-        clean_split = []
-        for val in first_split:
-            for param in val.split(','):
-                if param:
-                    clean_split.append(param)
-
         global count
-        if (count==2) :
+        self.parameter = self.parameterentry.get()
+        process = process_params(self.parameterentry.get(), self.independententry.get())
+        if not process[0]:
+            count = 1
             for x in range(self.boxnumber):
-                self.paramboxes[x].grid_forget()
-                self.paramlabel[x].grid_forget()
-                self.paramboxes[x].grid_rowconfigure(x, weight=1)
-
-
-            self.subframeright2.destroy()
-
-            self.subframeright2=tk.Frame(self.frameright, bg = '#FCF6F5')
-            self.subframeright2.place(in_ = self.frameright, relwidth=1, relheight=0.3, relx=0, rely=0.25)
-
-            self.boxnumber = len(clean_split)
-
-            self.paramlabel=[]
-            self.paramboxes=[]
-
+                    self.paramboxes[x].grid_forget()
+                    self.paramlabel[x].grid_forget()
+                    self.paramboxes[x].grid_rowconfigure(x, weight=1)
+    
+            # self.subframeright2.destroy()
             self.paramscrolly.destroy()
             self.anotherframe.destroy()
             self.paramcanvas.destroy()
+            self.inicialguesslabel.destroy()
+            self.secondary_window('ERROR', process[1])
+        else:
+            clean_split = process[1]
+            if (count==2) :
+                for x in range(self.boxnumber):
+                    self.paramboxes[x].grid_forget()
+                    self.paramlabel[x].grid_forget()
+                    self.paramboxes[x].grid_rowconfigure(x, weight=1)
+    
 
+                self.subframeright2.destroy()
+    
+                self.subframeright2=tk.Frame(self.frameright, bg = '#FCF6F5')
+                self.subframeright2.place(in_ = self.frameright, relwidth=1, relheight=0.3, relx=0, rely=0.25)
+    
+                self.boxnumber = len(clean_split)
+    
+                self.paramlabel=[]
+                self.paramboxes=[]
+    
+                self.paramscrolly.destroy()
+                self.anotherframe.destroy()
+                self.paramcanvas.destroy()
+    
+    
+                self.paramcanvas = tk.Canvas(self.subframeright2, highlightthickness=0)
+                self.paramcanvas.pack(side=tk.LEFT, fill = tk.BOTH, expand=1)
+    
+                self.paramscrolly = ttk.Scrollbar(self.subframeright2, orient = "vertical", command=self.paramcanvas.yview)
+                self.paramscrolly.pack(side=tk.RIGHT, fill="y")
+    
+                self.paramcanvas.configure(yscrollcommand=self.paramscrolly.set)
+                self.paramcanvas.bind('<Configure>', lambda e: self.paramcanvas.configure(scrollregion = self.paramcanvas.bbox("all")))
+    
+    
+                self.anotherframe=tk.Frame(self.paramcanvas)
+    
+                self.anotherframe.grid_columnconfigure(0, weight=1)
+                self.anotherframe.grid_columnconfigure(1, weight=1)
+                self.paramcanvas.create_window((0,0), window = self.anotherframe, anchor = "nw")
+    
+    
+                self.paramcanvas.bind_all('<MouseWheel>', lambda event: self.paramcanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
+    
+    
+    
+    
+                for x in range(self.boxnumber):
+                    self.anotherframe.grid_rowconfigure(x, weight=1)
+                    self.paramboxes.append(tk.Entry(self.anotherframe))
+                    self.paramboxes[x].grid(column = 1, row = x, pady=10, sticky='nsew')
+                    self.paramlabel.append(tk.Label(self.anotherframe, text = clean_split[x]+'\N{SUBSCRIPT ZERO}'))
+                    self.paramlabel[x].grid(column = 0, row = x, pady=10, sticky= 'nsew')
 
-            self.paramcanvas = tk.Canvas(self.subframeright2, highlightthickness=0)
-            self.paramcanvas.pack(side=tk.LEFT, fill = tk.BOTH, expand=1)
-
-            self.paramscrolly = ttk.Scrollbar(self.subframeright2, orient = "vertical", command=self.paramcanvas.yview)
-            self.paramscrolly.pack(side=tk.RIGHT, fill="y")
-
-            self.paramcanvas.configure(yscrollcommand=self.paramscrolly.set)
-            self.paramcanvas.bind('<Configure>', lambda e: self.paramcanvas.configure(scrollregion = self.paramcanvas.bbox("all")))
-
-
-            self.anotherframe=tk.Frame(self.paramcanvas)
-
-            self.anotherframe.grid_columnconfigure(0, weight=1)
-            self.anotherframe.grid_columnconfigure(1, weight=1)
-            self.paramcanvas.create_window((0,0), window = self.anotherframe, anchor = "nw")
-
-
-            self.paramcanvas.bind_all('<MouseWheel>', lambda event: self.paramcanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
-
-
-
-
-            for x in range(self.boxnumber):
-                self.anotherframe.grid_rowconfigure(x, weight=1)
-                self.paramboxes.append(tk.Entry(self.anotherframe))
-                self.paramboxes[x].grid(column = 1, row = x, pady=10, sticky='nsew')
-                self.paramlabel.append(tk.Label(self.anotherframe, text = clean_split[x]+'\N{SUBSCRIPT ZERO}'))
-                self.paramlabel[x].grid(column = 0, row = x, pady=10, sticky= 'nsew')
-
-
-
-        if (count == 1):
-
-            self.paramlabel=[]
-            self.paramboxes=[]
-
-            self.boxnumber = len(clean_split)
-
-            self.inicialguesslabel = tk.Label(self.subframeright1, text= "Initial Guess")
-            self.inicialguesslabel.place(rely=0.4, relwidth=0.3, relheight = 0.1, relx=0)
-
-            self.paramcanvas = tk.Canvas(self.subframeright2, highlightthickness=0)
-            self.paramcanvas.pack(side=tk.LEFT, fill = tk.BOTH, expand=1)
-
-
-
-
-            self.paramscrolly = ttk.Scrollbar(self.subframeright2, orient = "vertical", command=self.paramcanvas.yview)
-            self.paramscrolly.pack(side=tk.RIGHT, fill="y")
-
-            self.paramcanvas.configure(yscrollcommand=self.paramscrolly.set)
-            self.paramcanvas.bind('<Configure>', lambda e: self.paramcanvas.configure(scrollregion = self.paramcanvas.bbox("all")))
-
-
-            self.paramcanvas.bind_all('<MouseWheel>', lambda event: self.paramcanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
-
-            self.anotherframe=tk.Frame(self.paramcanvas)
-
-            self.paramcanvas.create_window((0,0), window = self.anotherframe, anchor = "nw")
-
-
-
-            for x in range(self.boxnumber):
-                self.paramboxes.append(tk.Entry(self.anotherframe))
-                self.paramboxes[x].grid(column = 1, row = x, pady=10, sticky='nsew')
-                self.paramlabel.append(tk.Label(self.anotherframe, text = clean_split[x]+'\N{SUBSCRIPT ZERO}'))
-                self.paramlabel[x].grid(column = 0, row = x, pady=10, sticky= 'nsew')
-
-        count = 2
+            if (count == 1):
+    
+                self.paramlabel=[]
+                self.paramboxes=[]
+    
+                self.boxnumber = len(clean_split)
+    
+                self.inicialguesslabel = tk.Label(self.subframeright1, text= "Initial Guess")
+                self.inicialguesslabel.place(rely=0.4, relwidth=0.3, relheight = 0.1, relx=0)
+    
+                self.paramcanvas = tk.Canvas(self.subframeright2, highlightthickness=0)
+                self.paramcanvas.pack(side=tk.LEFT, fill = tk.BOTH, expand=1)
+    
+    
+    
+    
+                self.paramscrolly = ttk.Scrollbar(self.subframeright2, orient = "vertical", command=self.paramcanvas.yview)
+                self.paramscrolly.pack(side=tk.RIGHT, fill="y")
+    
+                self.paramcanvas.configure(yscrollcommand=self.paramscrolly.set)
+                self.paramcanvas.bind('<Configure>', lambda e: self.paramcanvas.configure(scrollregion = self.paramcanvas.bbox("all")))
+    
+    
+                self.paramcanvas.bind_all('<MouseWheel>', lambda event: self.paramcanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
+    
+                self.anotherframe=tk.Frame(self.paramcanvas)
+    
+                self.paramcanvas.create_window((0,0), window = self.anotherframe, anchor = "nw")
+    
+    
+    
+                for x in range(self.boxnumber):
+                    self.paramboxes.append(tk.Entry(self.anotherframe))
+                    self.paramboxes[x].grid(column = 1, row = x, pady=10, sticky='nsew')
+                    self.paramlabel.append(tk.Label(self.anotherframe, text = clean_split[x]+'\N{SUBSCRIPT ZERO}'))
+                    self.paramlabel[x].grid(column = 0, row = x, pady=10, sticky= 'nsew')
+    
+            count = 2
 
     def fit_function(self, x, B):
         return eval(self.function)
